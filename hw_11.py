64296d693b53fc0edb073f9a7078ecae8da88fab
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-04-20 11:36:34 +0300

    final total version

--- hw_11.py
+++ hw_11.py
@@ -73,33 +73,21 @@ print(set1)
 
 # Task 14
 lst = [[1, None, 2, 3, 4+5j, 6], [1.0, 2.5, None, 3, 9, 4.0+5.2j, 6.1], ['1', '2', '3.6', None, '4+5.7j', '6']]
-int_lst = []
-float_lst = []
-str_lst = []
-res_int_lst = []
-res_float_lst = []
-res_str_lst = []
+clean_list = []
 for elem in lst:
-    types = []
-    [types.append((type(sym)).__name__) for sym in elem if (type(sym)).__name__]
+    types = [(type(sym)).__name__ for sym in elem if (type(sym)).__name__]
     single_types = []
     [single_types.append(i_types) for i_types in types if i_types not in single_types]
-    types_count = []
-    [types_count.append(types.count(element_type)) for element_type in single_types]
+    types_count = [types.count(element_type) for element_type in single_types]
     max_types_count = max(types_count)
     index_max = types_count.index(max(types_count))
     main_type = single_types[index_max]
-    separated_list = []
-    [separated_list.append(element) for element in elem if type(element).__name__ == main_type]
-    int_lst.extend(separated_list)
-    float_lst.extend(separated_list)
-    str_lst.extend(separated_list)
-[res_int_lst.append(k) for k in int_lst if type(k) == int]
-[res_float_lst.append(k) for k in float_lst if type(k) == float]
-[res_str_lst.append(k) for k in str_lst if type(k) == str]
-print("integer list copy", int_lst)
-print("float list copy", float_lst)
-print("string list copy", str_lst)
+    separated_list = [element for element in elem if type(element).__name__ == main_type]
+    clean_list.extend(separated_list)
+res_int_lst = [k for k in clean_list if type(k) == int]
+res_float_lst = [k for k in clean_list if type(k) == float]
+res_str_lst = [k for k in clean_list if type(k) == str]
+print("clean list copy", clean_list)
 print("list with integer type of numbers", res_int_lst)
 print("list with float type of numbers", res_float_lst)
 print("list with string type of numbers", res_str_lst)
